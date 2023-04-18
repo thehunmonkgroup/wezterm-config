aceaf86479ae47ddce37dc5091de233df66a6b74
Chad Phillips <chad@apartmentlines.com>
2023-04-18 00:27:16 -0700

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,59 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+--config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+config.color_scheme = 'Dracula (Official)'
+--config.color_scheme = 'GruvboxDark'
+
+config.font = wezterm.font 'UbuntuMono Nerd Font'
+
+config.skip_close_confirmation_for_processes_named = { 'flatpak-spawn' }
+
+local action = wezterm.action
+config.keys = {
+  {
+    key = 'LeftArrow',
+    mods = 'ALT',
+    action = action.ActivateTabRelative(-1),
+  },
+  {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = action.ActivateTabRelative(1),
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'SHIFT|ALT',
+    action = action.MoveTabRelative(-1),
+  },
+  {
+    key = 'RightArrow',
+    mods = 'SHIFT|ALT',
+    action = action.MoveTabRelative(1),
+  },
+  {
+    key = 'c',
+    mods = 'ALT',
+    action = wezterm.action.CopyTo 'Clipboard',
+  },
+  {
+    key = 'v',
+    mods = 'ALT',
+    action = wezterm.action.PasteFrom 'Clipboard',
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
